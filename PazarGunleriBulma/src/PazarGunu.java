9fc55342a52db723ae9beb6e70653b5368250d34
yektaonurylmz <40947943+yektaonurylmz@users.noreply.github.com>
2020-12-17 03:30:15 +0300

    Update

--- PazarGunleriBulma/src/PazarGunu.java
+++ PazarGunleriBulma/src/PazarGunu.java
@@ -7,10 +7,12 @@ public class PazarGunu {
         int day = 1;
         int startYear = 1900, endYear = 2000;
         int[] MouthDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        String[] MountName={"Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran", "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık"};
         for (int year = startYear; year <= endYear; year++) {
-            if (year % 4 == 0 && year % 100 != 0 ) {
+            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0 && year % 4000 != 0) {
                 MouthDays[1]=29;
-            }
+            }else
+                MouthDays[1]=28;
             int ay = 0;
 
             while (ay <=MouthDays.length-1) {
@@ -18,10 +20,10 @@ public class PazarGunu {
                 day +=  MouthDays[ay];
 
                 if(day%7 == 0 && ay==11){
-                    System.out.println("01.1." + (year+1) );
+                    System.out.println("01.Ocak." + (year+1) );
                 }
                 else if (day%7 == 0){
-                    System.out.println("01." + (ay+2) + "." + year );
+                    System.out.println("01." + (MountName[ay+1]) + "." + year );
                 }
                 ay++;
             }
@@ -63,4 +65,4 @@ public class PazarGunu {
         }
         */
     }
-}
+
