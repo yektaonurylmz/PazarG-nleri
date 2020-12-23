442fa1bb3e101eab460783ac99f398b8138ddbf1
yektaonurylmz <40947943+yektaonurylmz@users.noreply.github.com>
2020-12-23 15:05:34 +0300

    Update

--- PazarGunleriBulma/src/FindSundays.java
+++ PazarGunleriBulma/src/FindSundays.java
@@ -6,7 +6,7 @@ public class FindSundays implements IFindSundays {
     String[] MonthName={"Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran", "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık"};
 
     @Override
-    public boolean LastMonth(int a, int b) {
+    public boolean LastMonthOfYear(int a, int b) {
       return a%7==0 && b==11;
     }
     @Override
@@ -20,9 +20,9 @@ public class FindSundays implements IFindSundays {
     }
 
     @Override
-    public String Print(int month, String[] MountName, int year) {
+    public String PrintConsole(int month, String[] MonthName, int year) {
 
-        return "01." + (MountName[month+1]) +"."+ year ;
+        return "01." + (MonthName[month+1]) +"."+ year ;
     }
 
     @Override
@@ -31,7 +31,7 @@ public class FindSundays implements IFindSundays {
     }
 
     @Override
-    public void Hesapla(int day,int mouth, int startYear, int endYear) {
+    public void Calculate(int day, int mouth, int startYear, int endYear) {
 
         for (int year = startYear; year <= endYear; year++) {
 
@@ -46,12 +46,12 @@ public class FindSundays implements IFindSundays {
 
                 day = (day%7); // 12 bit'lik değeri aşmayacak şekilde düzenlendi;
 
-                switch (LastMonth(day,mouth)? 1:0){
+                switch (LastMonthOfYear(day,mouth)? 1:0){
                     case 1 : System.out.println(PrintFirstMonthOfYear(year));
                     break;
                     case 0:
                         switch (Sunday(day) ? 1 : 0) {
-                            case 1 -> System.out.println(Print(mouth, MonthName, year));
+                            case 1 -> System.out.println(PrintConsole(mouth, MonthName, year));
                         }
                 }
                 mouth++;
@@ -61,7 +61,7 @@ public class FindSundays implements IFindSundays {
 
     public static void main(String[] args) {
         FindSundays findSundays=new FindSundays();
-            findSundays.Hesapla(1,0,1900,2000);
+            findSundays.Calculate(1,0,1900,2000);
     }
 }
 
--- PazarGunleriBulma/src/IFindSundays.java
+++ PazarGunleriBulma/src/IFindSundays.java
@@ -1,9 +1,9 @@
 public interface IFindSundays {
-    boolean LastMonth(int day,int month);
+    boolean LastMonthOfYear(int day, int month);
     boolean LeapYear(int year);
-    String Print(int month, String[] monthName, int year);
+    String PrintConsole(int month, String[] monthName, int year);
     String PrintFirstMonthOfYear(int year);
-    void Hesapla(int day ,int month,int startYear,int endYear);
+    void Calculate(int day , int month, int startYear, int endYear);
     boolean Sunday(int day);
 
 }
